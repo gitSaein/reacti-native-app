728e500ae220420f9c02c87fe6f178084169e0ff
saeinlee <gitsaein@gmail.com>
2021-04-14 07:41:50 +0900

    main navigation

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import 'react-native-gesture-handler';
-import {StyleSheet} from 'react-native';
+import {Image} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -24,19 +24,101 @@ import SmsPhoneNumber from './src/screens/auth/SmsPhoneNumber';
 
 import Home from './src/screens/main/home';
 import Chat from './src/screens/main/chat';
+import Community from './src/screens/main/community';
+import MyPage from './src/screens/main/myPage';
+import Group from './src/screens/main/group';
 
 const Stack = createStackNavigator();
-
 const Tab = createBottomTabNavigator();
+
+const tabButtonRoutes = [
+  {
+    name: 'Home',
+    component: Home,
+    active: require('./src/assets/images/icon/Home.png'),
+    inactive: require('./src/assets/images/icon/HomeInactive.png'),
+  },
+  {
+    name: 'Community',
+    component: Community,
+    active: require('./src/assets/images/icon/Notifications.png'),
+    inactive: require('./src/assets/images/icon/NotificationsInactive.png'),
+  },
+  {
+    name: 'Group',
+    component: Group,
+    active: require('./src/assets/images/icon/Stream.png'),
+    inactive: require('./src/assets/images/icon/StreamInactive.png'),
+  },
+  {
+    name: 'Chat',
+    component: Chat,
+    active: require('./src/assets/images/icon/Chat.png'),
+    inactive: require('./src/assets/images/icon/ChatInactive.png'),
+  },
+  {
+    name: 'MyPage',
+    component: MyPage,
+    active: require('./src/assets/images/icon/Profile.png'),
+    inactive: require('./src/assets/images/icon/ProfileInactive.png'),
+  },
+];
 
 const HomeStackScreen = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Chat" component={Chat} />
+      {tabButtonRoutes.map(route => (
+        <Tab.Screen
+          key={`s-${route.name}`}
+          name={route.name}
+          component={route.component}
+          options={{
+            // tabBarBadge: 3,
+            tabBarLabel: route.name,
+            tabBarIcon: ({focused}) => {
+              return <Image source={focused ? route.active : route.inactive} />;
+            },
+          }}
+        />
+      ))}
     </Tab.Navigator>
   );
 };
+
+const tabNavRoutes = [
+  {
+    name: 'Welcome1',
+    component: Welcome1,
+  },
+  {
+    name: 'SignIn1',
+    component: SignIn1,
+  },
+  {
+    name: 'SignUp',
+    component: SignUp,
+  },
+  {
+    name: 'ForgotPassword',
+    component: ForgotPassword,
+  },
+  {
+    name: 'ResetPassword',
+    component: ResetPassword,
+  },
+  {
+    name: 'SmsPhoneNumber',
+    component: SmsPhoneNumber,
+  },
+  {
+    name: 'SmsCode',
+    component: SmsCode,
+  },
+  {
+    name: 'Home',
+    component: HomeStackScreen,
+  },
+];
 
 const App = () => {
   return (
@@ -44,14 +126,13 @@ const App = () => {
       <Stack.Navigator
         initialRouteName="Welcome1"
         screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Welcome1" component={Welcome1} />
-        <Stack.Screen name="SignIn1" component={SignIn1} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="ResetPassword" component={ResetPassword} />
-        <Stack.Screen name="SmsPhoneNumber" component={SmsPhoneNumber} />
-        <Stack.Screen name="SmsCode" component={SmsCode} />
-        <Stack.Screen name="Home" component={HomeStackScreen} />
+        {tabNavRoutes.map(route => (
+          <Stack.Screen
+            key={`n-${route.name}`}
+            name={route.name}
+            component={route.component}
+          />
+        ))}
       </Stack.Navigator>
     </NavigationContainer>
   );
